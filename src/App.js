5f3c32dd7201d629f88e1cf2187c2a2e4c38b62f
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-07-22 10:47:07 -0300

    Utilizando o React Fragments e Listas - Aula 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List'
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
       idade = " 29" 
       profissao = " Programador" 
       foto = "https://via.placeholder.com/150"/>
+
+      <List />
       
     </div>
   );
